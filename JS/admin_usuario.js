35512dff9812f688b9a4f721be581b68a85d7d28
Cabianzada <86447827+Cabianzada@users.noreply.github.com>
2021-10-02 11:25:35 -0500

    AjusteJS Admin Usuario V1

--- JS/admin_usuario.js
+++ JS/admin_usuario.js
@@ -1,51 +1,58 @@
-function nuevo() {
-      var id_usuario = document.getElementById("id_usuario").value;
-      var sel_rol = document.getElementById("sel_rol").value;
-      var sel_estado = document.getElementById("sel_estado").value;
-      var nombre_usuario = document.getElementById("nombre_usuario").value;
-      var no_identificacion = document.getElementById("no_identificacion").value;
-      var no_telefono = document.getElementById("no_telefono").value;
-      var correo_electronico = document.getElementById("correo_electronico").value;
-      var i = 1;
-      var fila = '<tr id="row' + i + '"><td>' + id_usuario + '</td><td>' + sel_rol + '</td><td>' + sel_estado + + '</td><td>' + nombre_usuario  + '</td><td>' + no_identificacion  + '</td><td>' + no_telefono  + '</td><td>' + correo_electronico + '</td><td><input type="checkbox" id="seleccionar' + i + '"></td></tr>';
-    
-      i++;
-    
-      $('#datos_usuarios tr:first').after(fila);
-        var nFilas = $("#datos_usuarios tr").length;
-        document.getElementById("id_usuario").value ="";
-        document.getElementById("sel_rol").value = "";
-        document.getElementById("sel_estado").value = "";
-        document.getElementById("nombre_usuario").value ="";
-        document.getElementById("no_identificacion").value = "";
-        document.getElementById("no_telefono").value = "";
-        document.getElementById("correo_electronico").value = "";
-        document.getElementById("id_usuario").focus();
-      }
-
-function seleccionar(){
-    document.getElementById("id_usuario").value ="fzapata";
-    document.getElementById("sel_rol").value = "Administrador";
-    document.getElementById("sel_estado").value = "Autorizado";
-    document.getElementById("nombre_usuario").value ="Fabian Zapata";
-    document.getElementById("no_identificacion").value = "1032420628";
-    document.getElementById("no_telefono").value = "3138832810";
-    document.getElementById("correo_electronico").value = "fabianza@outlook.com";
-    document.getElementById("id_usuario").focus();
-}
-function regresar(){
+function seleccionar(x){ 
+    document.getElementById("seleccionar1").checked = false
+    document.getElementById("seleccionar2").checked = false
+    document.getElementById("seleccionar3").checked = false
+    document.getElementById("seleccionar4").checked = false
+    document.getElementById("seleccionar5").checked = false
+    document.getElementById("seleccionar"+x).checked = true
+};
+function nuevo(){
     document.getElementById("id_usuario").value ="";
+    document.getElementById("contraseña").value = "";
     document.getElementById("sel_rol").value = "";
     document.getElementById("sel_estado").value = "";
     document.getElementById("nombre_usuario").value ="";
     document.getElementById("no_identificacion").value = "";
     document.getElementById("no_telefono").value = "";
     document.getElementById("correo_electronico").value = "";
+    document.getElementById("seleccionar1").checked = false
+    document.getElementById("seleccionar2").checked = false
+    document.getElementById("seleccionar3").checked = false
+    document.getElementById("seleccionar4").checked = false
+    document.getElementById("seleccionar5").checked = false
     document.getElementById("id_usuario").focus();
 }
 function buscar(){
-    document.getElementById("id_usuario").value = document.getElementById("micelda").innerText;
+    var x = document.getElementById("d_usuarios").rows.length + 1;
+    for (var i = 1; i < x; i++) {
+        if (document.getElementById("seleccionar"+i).checked == true){
+            x = i
+        };
+     }
+    document.getElementById("id_usuario").value =document.getElementById(x).cells[0].innerText;
+    document.getElementById("contraseña").value = "password";
+    document.getElementById("sel_rol").value = document.getElementById(x).cells[1].innerText;
+    document.getElementById("sel_estado").value = document.getElementById(x).cells[2].innerText;
+    document.getElementById("nombre_usuario").value =document.getElementById(x).cells[3].innerText;
+    document.getElementById("no_identificacion").value = document.getElementById(x).cells[4].innerText;
+    document.getElementById("no_telefono").value = document.getElementById(x).cells[5].innerText;
+    document.getElementById("correo_electronico").value = document.getElementById(x).cells[6].innerText;
+    document.getElementById("id_usuario").focus();
 }
 function guardar(){
+    var x = document.getElementById("d_usuarios").rows.length + 1;
+    for (var i = 1; i < x; i++) {
+        if (document.getElementById("seleccionar"+i).checked == true){
+            x = i
+        };
+     }
+    document.getElementById(x).cells[0].innerText = document.getElementById("id_usuario").value;
+    document.getElementById(x).cells[1].innerText = document.getElementById("sel_rol").value;
+    document.getElementById(x).cells[2].innerText = document.getElementById("sel_estado").value;
+    document.getElementById(x).cells[3].innerText = document.getElementById("nombre_usuario").value;
+    document.getElementById(x).cells[4].innerText = document.getElementById("no_identificacion").value;
+    document.getElementById(x).cells[5].innerText = document.getElementById("no_telefono").value;
+    document.getElementById(x).cells[6].innerText = document.getElementById("correo_electronico").value;
     alert('Usuario: '+ document.getElementById("id_usuario").value + ' Rol Actualizado: ' + document.getElementById("sel_rol").value + ' Estado Actualizado: '+ document.getElementById("sel_estado").value);
+    nuevo()
 }
